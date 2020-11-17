febf7b15ddaf433a895d8dcedd4079852d0ba117
Parker McCrary <parkertomm3@gmail.com>
2020-11-17 14:22:08 -0500

    Nav component built

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,1 +1,15 @@
+// Import libraries
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Nav() {
+    <nav className="main-nav">
+        <ul>
+            <li><NavLink to="/naruto">Naruto</NavLink></li>
+            <li><NavLInk to="/fitness">Fitness</NavLInk></li>
+            <li><NavLink to="/supreme">Supreme</NavLink></li>
+        </ul>
+    </nav>
+}
+
 export default Nav
