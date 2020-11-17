fe2bcbc5830cc6f9b8230c4b1f192b41da63a246
Parker McCrary <parkertomm3@gmail.com>
2020-11-17 14:04:13 -0500

    Import React router + main app render skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 // Import libraries
 import React, { Component } from 'react'
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
 
 // Import API key from config.js
 import apiKey from './config'
 
 // Import App components
-import SearchForm from './components/Search';
+import Search from './components/Search';
 import Nav from './components/Nav';
 import PhotoContainer from './components/PhotoContainer';
 import Error404 from './components/PageNotFound';
@@ -15,9 +21,15 @@ class App extends Component {
   
   render() {
     return (
-      <div className="main-container">
-        <h1>Hello World</h1>
-      </div>
+      <BrowserRouter>
+        <div className="main-container">
+          <Search />
+          <Nav />
+          <Switch>
+            <Route exact path="/" render = { () => <Redirect to="/" />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
