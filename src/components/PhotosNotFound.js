f7bcd38d6c2171932933a75a3e9018a25594175e
Parker McCrary <parkertomm3@gmail.com>
2020-11-18 14:24:52 -0500

    PhotosNotFound component build

--- src/components/PhotosNotFound.js
+++ src/components/PhotosNotFound.js
@@ -1,1 +1,13 @@
+// Import libraries
+import React from 'react';
+
+function PhotosNotFound(){
+    return(
+        <div className="not-found">
+            <h1>No Results Found</h1>
+            <h3>That search did not return any results, please try again!</h3>
+        </div>
+    );
+}
+
 export default PhotosNotFound
