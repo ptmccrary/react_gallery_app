862df01e8cf1d0175368640df2613836c6fffd36
Parker McCrary <parkertomm3@gmail.com>
2020-11-18 11:54:16 -0500

    PhotoContainer component build

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,1 +1,30 @@
+// Import libraries
+import React, { Component } from 'react';
+import Photo from './Photo';
+import PageNotFound from './PageNotFound';
+
+class PhotoContainer extends Component {
+    getPhotos() {
+        let results = this.props.data;
+        let images;
+
+        if(results.length > 0) {
+            images = results.map(image => <Photo url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} key={image.id} />);
+        } else {
+            images = <PageNotFound />;
+        }
+        
+        return images;
+    }
+
+    render() {
+        return(
+            <div className="photo-container">
+                <h2>{ this.props.query }</h2>
+                <ul>{ this.getPhotos() }</ul>
+            </div>
+        );
+    }
+}
+
 export default PhotoContainer
