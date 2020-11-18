754f4c474a989a419c883e35561f2afabdc360ef
Parker McCrary <parkertomm3@gmail.com>
2020-11-18 14:28:35 -0500

    PageNotFound component build

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,1 +1,12 @@
+// Import libraries
+import React from 'react';
+
+const PageNotFound = () => (
+    <div className="error-container">
+        <h2>ERROR 404</h2>
+        <h3>Page Not Found</h3>
+        <p>Check your search bar to see if you have the correct URL</p>
+    </div>
+)
+
 export default PageNotFound
